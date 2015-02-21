2fcc10a1802f0f4b28422578e9abf36f9b24dcf5
Christian Gunderman <christian@sonofboss.cwru.edu>
2015-02-21 17:36:10 -0500

    Android project Gradle build integration and Shared library dependency.

--- android/src/com/pinata/android/Main.java
+++ android/src/com/pinata/android/Main.java
@@ -3,8 +3,9 @@ package com.pinata.android;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Main extends Activity
-{
+import com.pinata.shared.ApiStatus;
+
+public class Main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
