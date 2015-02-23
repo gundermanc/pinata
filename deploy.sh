ba257cc1567d755c1681ecb7eb5352218cd17ae1
Christian Gunderman <gundermanc@gmail.com>
2015-02-23 13:24:14 -0500

    Deploy.sh: added support for different Tomcat versions and prompts.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,53 @@
-#!/bin/sh
+#!/bin/bash
 
 # Pinata deployment script
 # (C) 2015 Christian Gunderman
 
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 
-gradle clean build test
+clear
+echo "Pinata Deployment Script"
+echo "(C) 2015 Christian Gunderman"
+echo 
 
+# Get tomcat version.
+echo "What version of tomcat are you using ?"
+echo " 1. tomcat6"
+echo " 2. tomcat7"
+echo " 3. tomcat8"
+read PINATA_MENU
+
+if [ $PINATA_MENU = '1' ]
+then
+    PINATA_TOMCAT_VERSION="tomcat6"
+elif [ $PINATA_MENU = '2' ]
+then
+    PINATA_TOMCAT_VERSION="tomcat7"
+elif [ $PINATA_MENU = '3' ]
+then
+    PINATA_TOMCAT_VERSION="tomcat8"
+else
+    echo "Invalid menu option."
+    echo "Aborting."
+    exit
+fi
+
+# Prompt for rebuild.
+echo "Rebuild ?"
+echo "(Y, n)"
+read PINATA_YN
+if [ $PINATA_YN = 'Y' ]
+then
+    echo "Cleaning, building, testing..."
+    gradle clean build test
+else
+    echo "Skipping rebuild..."
+fi
+
+# Install if Rebuild was successful.
 if [ $? -eq 0 ]
 then
-    sudo cp -f service/build/libs/service.war /var/lib/tomcat6/webapps/ROOT.war
-#    cd /usr/share/tomcat8/lib
- #   sudo wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.6/mysql-connector-java-5.1.6.jar
+    echo "Installing..."
+    sudo cp -f service/build/libs/service.war /var/lib/$PINATA_TOMCAT_VERSION/webapps/ROOT.war
 fi
+echo "Finished."
